d2327086a14d57febda6924baeec682b7de8fe4e
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2024-02-06 20:03:08 +0530

    Create StudentRepository.java

---
+++ src/main/java/com/saqlain/studentsystem/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.saqlain.studentsystem.repository;
+
+
+
+import com.saqlain.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+}
