33b04f3b507ecabdf0489ce1e7a3d3584bd91fab
Shaqlain07 <104688734+Shaqlain07@users.noreply.github.com>
2024-02-06 19:59:12 +0530

    Create StudentsystemApplication.java

---
+++ src/main/java/com/saqlain/studentsystem/StudentsystemApplication.java
@@ -0,0 +1,15 @@
+package com.saqlain.studentsystem;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsystemApplication.class, args);
+	}
+
+}
